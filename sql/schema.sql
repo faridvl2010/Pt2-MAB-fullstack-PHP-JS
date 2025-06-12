dbc369421b57106d871ffd1a82ab7a8aba04355f
faridvl2010 <faridvl2010@gmail.com>
2025-06-12 17:30:33 -0500

    Bd original

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE usuarios (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50),
-  password_hash VARCHAR(255)
+  username VARCHAR(50) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE descargas_pdf (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
+  user_id INT NOT NULL,
   pais VARCHAR(100),
   ruta_archivo VARCHAR(255),
   fecha DATETIME,
